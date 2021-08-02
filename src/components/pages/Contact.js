8694f8d10e1cf95d4c3b2ee35059767786640d2c
cgorrino <cgorrino03@gmail.com>
2021-08-02 16:17:47 -0700

    Improving the Contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,14 +5,15 @@ export default function Contact() {
         <div>
             <h3>Get in contact</h3>
             <p>Please leave your contact details, and I'll get back to you</p>
-            <form>
-                <label for="conactName">Name:</label>
-                <input type="text" id="contactName" name="contactName" required></input>
-                <label for="contactEmail">Email:</label>
-                <input type="email" id="contactEmail" name="contactEmail" required></input>
-                <label for="message">Message:</label>
-                <input type="textArea" id="message" name="message"></input>
-                <button type="button" value="Submit"></button>
+            <form className="mb-3">
+                <label className="form-label" for="conactName">Name:</label>
+                <input type="text" className="form-control" id="contactName" name="contactName" required></input>
+                <label className="form-label" for="contactEmail">Email:</label>
+                <input type="email" className="form-control" id="contactEmail" name="contactEmail" required></input>
+                <label className="form-label" for="message">Message:</label>
+                <input type="textArea" className="form-control" id="message" name="message"></input>
+                <button type="submit" class="btn btn-primary">Submit</button>
+
             </form>
         </div>
     )
