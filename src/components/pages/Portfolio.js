98549c0c30d47b05b659307f835813a450184471
cgorrino <cgorrino03@gmail.com>
2021-08-03 11:46:09 -0700

    Passing project data as props to Project component

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Project from '../Project';
 
-export default function Portfolio () {
+const examples = [
+    {
+        id: 1,
+        name: "Project 2 - Recipe Roulette",
+        github: "https://cristina-gorrino.github.io/recipe-roulette/",
+        deployed: "https://github.com/cristina-gorrino/recipe-roulette",
+        image: "../../assets/images/recipe-roulette.png",
+        alt: "screenshot of recipe roulette app"
+
+    }
+]
+
+export default function Portfolio ({id, name, github, deployed, image}) {
     return (
         <div>
-            <p>Portfolio coming soon!</p>
+            <h3>Portfolio coming soon!</h3>
+            {examples.map((project) => (
+                <Project id={project.id} name={project.name} github={project.github} deployed={project.deployed} image={project.image} alt={project.alt} />
+            ))}
+            
         </div>
     )
 }
